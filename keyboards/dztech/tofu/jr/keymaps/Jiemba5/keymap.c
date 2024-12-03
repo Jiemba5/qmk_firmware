08d19872b73529120579f849bf8278582c507a79
Jiemba5 <tijnschram@gmail.com>
2024-12-03 21:32:14 +0100

    temp

--- keyboards/dztech/tofu/jr/keymaps/Jiemba5/keymap.c
+++ keyboards/dztech/tofu/jr/keymaps/Jiemba5/keymap.c
@@ -13,95 +13,192 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+#define DRIVER_LED_TOTAL 48
 #include QMK_KEYBOARD_H
-#define _APEX 3
 // Timer and counter variables
 static uint16_t tap_timer = 0;
 static uint8_t tap_count = 0;
 #define TAP_TIME 200 // Timeframe in milliseconds
-
+#define _DEFAULT 0
+#define _APEX 3
 // Define custom keycodes
 enum custom_keycodes {
     TAP_HANDLER = SAFE_RANGE,
     SWITCH_APEX = SAFE_RANGE+1,
 };
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_65_ansi(
-        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
-        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
-        KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   SWITCH_APEX,
-        KC_LCTL,  KC_LGUI, KC_LALT,                            KC_SPC,           KC_RALT, KC_RGUI, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
-    ),
-    [1] = LAYOUT_65_ansi(
-        QK_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_HOME,
-        _______, RM_TOGG, RM_NEXT, RM_HUEU, RM_HUED, RM_SATU, RM_SATD, RM_VALU, RM_VALD, _______, KC_PSCR, KC_SCRL, KC_PAUS, QK_BOOT, KC_PGUP,
-        KC_CAPS, RM_SPDU, RM_SPDD, _______, _______, _______, _______, _______, _______, _______, _______, _______,         EE_CLR,  KC_PGDN,
-        KC_LSFT, _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______, _______,          KC_VOLU, KC_MUTE,
-        _______, _______, _______,                            _______,          _______, _______, _______,          KC_MPRV, KC_VOLD, KC_MNXT
-    ),
-    [2] = LAYOUT_65_ansi(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
-        _______, _______, _______,                            _______,          _______, _______, _______,          _______, _______, _______
-    ),
-    [_APEX] = LAYOUT_65_ansi(
-        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
-        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
-        KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
-        KC_LSFT,  KC_Z,    KC_X,    TAP_HANDLER,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   SWITCH_APEX,
-        KC_LCTL,  KC_LGUI, KC_LALT,                            KC_SPC,           KC_RALT, KC_RGUI, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
-    ),
-};
+typedef struct{
+    int r;
+    int b;
+    int g;
+} MY_RGB;
 
-// Process custom keycode
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case TAP_HANDLER:
-        {
-            if (record->event.pressed) {
-                tap_timer = timer_read();
-                tap_count++;
-            }
-            return false;
-        }
-        case SWITCH_APEX:
-        {
-            if (record->event.pressed) {
-                if (layer_state & (1 << _APEX)) {
-                    layer_off(_APEX);
-                } else {
-                    layer_on(_APEX);
-                };
-            }
-        }
-        default:
-            return true;
+typedef struct {
+    int period;
+    int start;
+} TimeThing;
+
+typedef struct {
+    int index;
+    MY_RGB rgb;
+    TimeThing timer;
+} ColorKeys;
+
+
+
+typedef enum {
+    DEFAULT = 0,
+    APEX = 4,
+} Profile;
+
+Profile current_profile = DEFAULT;
+
+void switchToProfile(Profile new_profile){
+    if (current_profile == new_profile) {
+        layer_on(_DEFAULT);
     }
+
+    switch (new_profile) {
+        case APEX:
+            layer_on(_APEX);
+            current_profile = APEX;
+        default:
+            layer_on(_DEFAULT);
+            current_profile = DEFAULT;
+    }
+    
 }
 
 
 
-void matrix_scan_user(void) {
-   if (timer_elapsed(tap_timer) >= TAP_TIME && tap_count > 0) {
-        if (tap_count == 1) {
-            send_string(",");
-        } else if (tap_count == 2) {
-            send_string(".");
-        } else if (tap_count == 3) {
-            send_string("/");
-        } else {
-            send_string("'");
-        }
-        tap_count = 0;
-    }
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_DEFAULT] = LAYOUT_65_ansi(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+                         KC_HOME, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,
+                         KC_BSLS, KC_PGUP, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN,
+                         KC_QUOT, KC_ENT, KC_PGDN, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
+                         KC_SLSH, KC_RSFT, KC_UP, SWITCH_APEX, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_RGUI,
+                         KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+    [1] = LAYOUT_65_ansi(QK_GESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
+                         KC_DEL, KC_HOME, _______, RM_TOGG, RM_NEXT, RM_HUEU, RM_HUED, RM_SATU, RM_SATD, RM_VALU,
+                         RM_VALD, _______, KC_PSCR, KC_SCRL, KC_PAUS, QK_BOOT, KC_PGUP, KC_CAPS, RM_SPDU, RM_SPDD,
+                         _______, _______, _______, _______, _______, _______, _______, _______, _______, EE_CLR,
+                         KC_PGDN, KC_LSFT, _______, _______, _______, _______, _______, NK_TOGG, _______, _______,
+                         _______, _______, _______, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______,
+                         _______, _______, KC_MPRV, KC_VOLD, KC_MNXT),
+    [2] = LAYOUT_65_ansi(_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                         _______, _______, _______, _______, _______, _______, _______, _______),
+    [_APEX] = LAYOUT_65_ansi(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,
+                             KC_BSPC, KC_HOME, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
+                             KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J,
+                             KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN, KC_LSFT, KC_Z, KC_X, TAP_HANDLER, KC_V,
+                             KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, SWITCH_APEX, KC_LCTL, KC_LGUI,
+                             KC_LALT, KC_SPC, KC_RALT, KC_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+};
+void keyboard_post_init_user(void) {
+    rgb_matrix_mode(RGB_MATRIX_HUE_PENDULUM);
+}
 
-    if (tap_count >= 4) {
-        tap_count = 0;
-        send_string("'");
+#define KEYS_APEX 4
+ColorKeys apex_color_map[KEYS_APEX] = {
+    {52, {0,0,255}, {500, 0}}, // ,
+    {53, {0,0,255}, {500, 0}}, // .
+    {54, {0,0,255}, {500, 0}}, // /
+    {41, {0,0,255}, {500, 0}}};// '
+
+MY_RGB APEX_RGB = {0, 255, 0};
+MY_RGB DEFAULT_RGB = {200, 200, 200};
+
+void displayRgbMatrix(ColorKeys* color_keys, MY_RGB default_rgb)
+{
+    for (int i = 0; i < KEYS_APEX;i++){
+        if (timer_elapsed(color_keys[i].timer.start)<= color_keys[i].timer.period){
+            rgb_matrix_set_color(color_keys[i].index, color_keys[i].rgb.r, color_keys[i].rgb.g, color_keys[i].rgb.b);
+        } else {
+            rgb_matrix_set_color(color_keys[i].index, default_rgb.r, default_rgb.g, default_rgb.b);
+        }
     }
+}
+
+bool rgb_matrix_indicators_user(void) {
+
+    switch (current_profile) {
+        case DEFAULT:
+        {
+            // displayRgbMatrix(default_color_map, DEFAULT_RBG);
+        }
+        case APEX:
+        {
+            displayRgbMatrix(apex_color_map, APEX_RGB);
+        }
+
+        default:
+            return true;
+        }
+    return true;
+}
+
+// Process custom keycode
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        switch (keycode) {
+            case TAP_HANDLER:
+            {
+                tap_timer = timer_read();
+                tap_count++;
+                
+                return false;
+            }
+            case SWITCH_APEX:
+            {
+                switchToProfile(APEX);
+                return false;
+            }
+            default:
+                return true;
+        }
+    }
+    return true;
+}
+
+ColorKeys tickApexColorKey(ColorKeys color_key){
+    ColorKeys new_color_key = {color_key.index, color_key.rgb, {color_key.timer.period, timer_read()}};
+    return new_color_key;
+}
+
+void matrix_scan_user(void) {
+    if (current_profile == APEX) {
+        if (timer_elapsed(tap_timer) >= TAP_TIME && tap_count > 0) {
+            if (tap_count == 1) {
+                send_string(",");
+                apex_color_map[0] = tickApexColorKey(apex_color_map[0]);
+            }
+            else if (tap_count == 2)
+            {
+                send_string(".");
+                apex_color_map[1] = tickApexColorKey(apex_color_map[1]);
+            }
+            else if (tap_count == 3)
+            {
+                send_string("/");
+                apex_color_map[2] = tickApexColorKey(apex_color_map[2]);
+            }
+            else
+            {
+                send_string("'");
+                apex_color_map[3] = tickApexColorKey(apex_color_map[3]);
+            }
+            tap_count = 4;
+        }
+        if (tap_count >= 4) {
+            tap_count = 0;
+            send_string("'");
+            apex_color_map[3] = tickApexColorKey(apex_color_map[3]);
+        }
+    }
+    
 }
--- keyboards/dztech/tofu/jr/rules.mk
+++ keyboards/dztech/tofu/jr/rules.mk
@@ -1,2 +1,3 @@
 DEFAULT_FOLDER = dztech/tofu/jr/v1
-VIA_ENABLE = yes
+# VIA_ENABLE = yes
+# RGBLIGHT_ENABLE = yes
